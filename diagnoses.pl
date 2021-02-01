c4842e8a6264f7ef534af0a3c6c47b4c019495eb
Christopher Lim <lim.christopher.go@gmail.com>
2021-02-01 09:51:46 +0800

    Add the asking of symptoms

--- diagnoses.pl
+++ diagnoses.pl
@@ -26,6 +26,96 @@ age(Name, Number).
    low_blood_pressure
 */
 
+symptom(Patient, shortness_of_breath):-
+    write("Does ", Patient, " have shortness of breath?")
+    response(Reply),
+    Reply = 'y'.
+
+
+symptom(Patient, runny_nose):-
+    write("Does ", Patient, " have runny nose?")
+    response(Reply),
+    Reply = 'y'.
+    
+symptom(Patient, chest_pain):-
+    write("Does ", Patient, " have chest pain?")
+    response(Reply),
+    Reply = 'y'.
+    
+symptom(Patient, chest_discomfort):-
+    write("Does ", Patient, " have chest discomfort?")
+    response(Reply),
+    Reply = 'y'.
+    
+symptom(Patient, wheezing_when_exhaling):-
+    write("Does ", Patient, " wheeze while exhaling?")
+    response(Reply),
+    Reply = 'y'.
+    
+symptom(Patient, cough_with_phlegm):-
+    write("Does ", Patient, " have cough with phlegm?")
+    response(Reply),
+    Reply = 'y'.
+    
+symptom(Patient, dry_cough):-
+    write("Does ", Patient, " have dry cough?")
+    response(Reply),
+    Reply = 'y'.
+    
+symptom(Patient, not_mentally_alert):-
+    write("Is ", Patient, " not mentally alert sometimes?")
+    response(Reply),
+    Reply = 'y'.
+    
+symptom(Patient, fatigue):-
+    write("Does ", Patient, " experience fatigue?")
+    response(Reply),
+    Reply = 'y'.
+    
+symptom(Patient, fever):-
+    write("Does ", Patient, " have fever?")
+    response(Reply),
+    Reply = 'y'.
+    
+symptom(Patient, nausea):-
+    write("Does ", Patient, " experience nausea?")
+    response(Reply),
+    Reply = 'y'.
+    
+symptom(Patient, loss_of_appetite):-
+    write("Does ", Patient, " sometimes have loss of appetite?")
+    response(Reply),
+    Reply = 'y'.
+    
+symptom(Patient, anxiety):-
+    write("Does ", Patient, " have anxiety?")
+    response(Reply),
+    Reply = 'y'.
+    
+symptom(Patient, bluish_lips):-
+    write("Does ", Patient, " have bluish lips?")
+    response(Reply),
+    Reply = 'y'.
+    
+symptom(Patient, palpitations):-
+    write("Does ", Patient, " have palpitations?")
+    response(Reply),
+    Reply = 'y'.
+    
+symptom(Patient, fast_heartbeat):-
+    write("Does ", Patient, " have fast heartbeat?")
+    response(Reply),
+    Reply = 'y'.
+    
+symptom(Patient, bone_pain):-
+    write("Does ", Patient, " have pain in the bone?")
+    response(Reply),
+    Reply = 'y'.
+    
+symptom(Patient, low_blood_pressure):-
+    write("Does ", Patient, " have low blood pressure?")
+    response(Reply),
+    Reply = 'y'.
 
 hypothesis(Patient, asthma):-
     symptom(Patient, shortness_of_breath),
@@ -73,3 +163,13 @@ hypothesis(Patient, interstitial_lung_disease):-
 hypothesis(Patient, pneumothorax):-
     symptom(Patient, ),
     symptom(Patient, ).
+
+response(Reply):-
+    readchar(Reply),
+    write(Reply), nl.
+
+start:-
+    write("What is the name of the patient? "),
+    readln(Patient),
+    hypothesis(Patient, Disease),
+    write(Patient, " probably has ", Disease, "."), nl.
